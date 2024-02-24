159693a45008f234dd4dd7994b64453b6b1502c9
silvia <djmery76@gmail.com>
2024-02-24 17:14:04 +0100

    Control props alternativo

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -9,37 +9,20 @@ export const useShoppingCart = () => {
 
         setShoppingCart(oldShoppingCart => {
 
-            // si el producto por el id existe entonces es el productIncart sino lo creará
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
+            if (count === 0) {
+                //Eliminamos el objeto mediante desestructuración, el operador rest es como quiero que quede mi estado
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+                console.log({ toDelete });
+                console.log({ ...rest });
                 return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
+                    ...rest
                 }
             }
-            //Borrar el producto, el artículo no existe, o la suma es 0 o menor a 0
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            console.log({ toDelete });
+
             return {
-                ...rest
+                ...oldShoppingCart,
+                [product.id]: { ...product, count }
             }
-
-            // if (count === 0) {
-            //     //Eliminamos el objeto mediante desestructuración, el operador rest es como quiero que quede mi estado
-            //     const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            //     console.log({ toDelete });
-            //     console.log({ ...rest });
-            //     return {
-            //         ...rest
-            //     }
-            // }
-
-            // return {
-            //     ...oldShoppingCart,
-            //     [product.id]: { ...product, count }
-            // }
         })
     }
 
